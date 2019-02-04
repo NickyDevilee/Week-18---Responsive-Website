82cc6978f061846809689db4e629052ebd30c25f
Unknown <99047338@mydavinci.nl>
2019-02-04 11:33:45 +0100

    afgemaakt.

---
+++ app.js
@@ -0,0 +1,12 @@
+// Nicky Devilee | 99047338
+// Bol4 applicatieontwikkelaar
+// Blok 2
+// Week 18 - Responsive website / mobile first
+
+function dropDown1 () {
+   document.getElementById('contactDropdown').classList.toggle('show');
+}
+
+function dropDown2 () {
+   document.getElementById('linksDropdown').classList.toggle('show');
+}
